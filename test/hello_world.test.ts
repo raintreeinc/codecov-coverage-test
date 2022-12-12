a1621ba7f61324e2d63551b98844017310eb4128
Chamal Jayasinghe <s16380@sci.pdn.ac.lk>
2022-12-12 20:01:30 +0530

    adding helloworl.test.ts file

--- test/hello_world.test.ts
+++ test/hello_world.test.ts
@@ -1,5 +1,13 @@
 const greeting = 'world';
 
 export function hello(world: string = greeting): string {
-    return `Hello ${world}! `;
+    return `Hello ${world}!`;
 }
+
+// import { hello } from "../src/hello_world";
+
+describe("test hello", () => {
+    it("should return hello world", () => {
+        expect(hello()).toBe("Hello world!");
+    });
+});
--- test/hello_world2.test.ts
+++
@@ -1,8 +0,0 @@
-const greeting = 'world';
-
-export function hello(world: string = greeting): string {
-    if (world == "Bob") {
-        return "Hiya Bob!";
-    }
-    return `Hello ${world}!`;
-}
